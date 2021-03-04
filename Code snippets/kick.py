dc613d562978ae6171ec873dd59995f07afd5198
Moses-Gaming <80053750+Moses-Gaming@users.noreply.github.com>
2021-03-04 14:35:01 -0400

    Update kick.py

--- Code snippets/kick.py
+++ Code snippets/kick.py
@@ -1,1 +1,6 @@
-
+@client.command(name='kick', pass_context = True)
+@commands.has_permissions(kick_members=True)
+async def kick(context, member: discord.Member):
+  
+  await member.kick()
+  await context.send('user ' + member.display_name + ' has been kicked.')
